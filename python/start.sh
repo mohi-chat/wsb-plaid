8f3615fbab1c65429711d73f74449497c30b538d
Mohinish <mohinishchatterjee@gmail.com>
2021-06-25 18:27:25 -0500

    Configured gunicorn to run in deamon mode

--- python/start.sh
+++ python/start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo nohup gunicorn --bind 0.0.0.0:8000 --worker-class 'uvicorn.workers.UvicornWorker' --workers 4 server:app
+gunicorn --bind 0.0.0.0:8000 --worker-class 'uvicorn.workers.UvicornWorker' --workers 4 server:app --daemon
